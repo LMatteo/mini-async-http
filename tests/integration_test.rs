4004425b28acf04d9cc2f08b10c5adc192cad8cf
Lucas Matteo <lulu.matteo@gmail.com>
2020-08-09 12:17:22 +0200

    Add parallel test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,8 @@ use mini_async_http::Request;
 
 use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex};
+use std::net::TcpStream;
+use std::io::Write;
 
 mod common;
 
@@ -37,4 +39,41 @@ fn simple_post_request() {
 
         assert_eq!("POST", body);
     })
+}
+
+#[test]
+fn multiple_get() {
+    run_test(|config| {
+        let mut handles = Vec::new();
+
+        const NB_PARALLEL : i8 = 20;
+        const NB_REQUEST : i8 = 20;
+
+        for i in 0..NB_PARALLEL {
+            let config = config.clone();
+            handles.push(std::thread::spawn(move || {
+                let addr = config.http_addr.as_str();
+                let uri : http_req::uri::Uri = addr.parse().unwrap(); 
+                let mut stream = TcpStream::connect((uri.host().unwrap(),uri.corr_port())).unwrap();
+
+                for i in 0..NB_REQUEST {
+                    let mut writer = Vec::new();
+
+                    let response = http_req::request::RequestBuilder::new(&uri)
+                        .method(http_req::request::Method::GET)
+                        .header("Connection", "Keep-Alive")
+                        .send(&mut stream, &mut writer)
+                        .unwrap();
+                    
+                    let body = std::str::from_utf8(&writer).unwrap();
+                    assert_eq!("GET", body);
+                }
+            }));
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    })
+    
 }
